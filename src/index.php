16fbfb74bc2c5beca33fd073f439aa211df84f34
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-14 17:19:58 +0530

    list all products in mobile subcategory in format

--- src/index.php
+++ src/index.php
@@ -102,5 +102,36 @@ $products = array(
        }
        ?>
     </table>
+
+    <br>
+    
+    <table>
+        <tr>
+            <th>Category</th>
+            <th>Subcategory</th>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Brand</th>
+        </tr>
+
+        <?php
+        foreach ($products as $key1 => $value1) {
+          foreach ($value1 as $key2 => $value2) {
+              if ($key2 == "Mobile") {
+                  foreach ($value2 as $val) {
+                    echo "<tr>";
+                       echo "<td>$key1</td>";
+                       echo "<td>$key2</td>";
+                       echo "<td>".$val['id']."</td>";
+                       echo "<td>".$val['name']."</td>";
+                       echo "<td>".$val['brand']."</td>";
+                    echo "</tr>";;
+
+                  }
+              }
+          }
+       }
+       ?>
+    </table>
 </body>
 </html>
